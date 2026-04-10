1e75e45eb4ad68eae6064fc43828c84c1d94ef20
Bella Nur Hidayah <bellanurhidayah08@gmail.com>
2026-04-10 13:41:56 +0700

    perbaikan auth dan controller

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateStudentDto {
-  nis: string;
-  name: string;
-  email: string;
-  kelas: string;
-  jurusan: string;
+  @ApiProperty({ example: '12345', description: 'Nomor Induk Siswa' })
+  nis?: string;
+
+  @ApiProperty({ example: 'Bella', description: 'Nama Lengkap Siswa' })
+  name?: string;
+
+  @ApiProperty({ example: 'bella@mail.com', description: 'Email Aktif' })
+  email?: string;
+
+  @ApiProperty({ example: 'XII-RPL', description: 'Kelas Siswa' })
+  kelas?: string;
+
+  @ApiProperty({ example: 'Rekayasa Perangkat Lunak', description: 'Jurusan Siswa' })
+  jurusan?: string;
 }
