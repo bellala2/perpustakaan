40959178b9eaf786180141a51e74fa945724b15f
Bella Nur Hidayah <bellanurhidayah08@gmail.com>
2026-04-17 00:22:30 +0700

    prepare deployment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,6 +17,6 @@ import { PrismaModule } from '../../prisma/prisma.module';
   ],
   controllers: [AuthController],
   providers: [AuthService,JwtStrategy,],
-  exports: [JwtModule,PassportModule,],
+  exports: [AuthService,JwtModule,PassportModule,],
 })
 export class AuthModule { }
